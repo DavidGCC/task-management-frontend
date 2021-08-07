7c7a70699b6e47000975ce0a6190e5203cd8b0a2
DavidGCC <DavidGCC@users.noreply.github.com>
2021-08-07 22:00:41 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.71289255519cb3683192b714e4e1505d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35083b0620c29d35b673ce3d1b962186",
+    "url": "https://davidgcc.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "d776eab6bb4d37a4ebb9",
+    "url": "https://davidgcc.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "a2d0c4e72d161208b660",
+    "url": "https://davidgcc.github.io/task-management-frontend/static/js/2.ed47dcf2.chunk.js"
+  },
+  {
+    "revision": "d776eab6bb4d37a4ebb9",
+    "url": "https://davidgcc.github.io/task-management-frontend/static/js/main.8de7baa6.chunk.js"
+  },
+  {
+    "revision": "85ac804806479d5e76f7",
+    "url": "https://davidgcc.github.io/task-management-frontend/static/js/runtime~main.0e03dc60.js"
+  }
+]);
